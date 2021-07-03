f1188956e6ef657ec12d18bcb14dbd554180f535
Bruno Coimbra <bruno.coimbra.ribeiro@hotmail.com>
2021-07-03 12:34:52 -0300

    Add final GameWorld tests;
    Fix invalid move not returning the control to the last player

--- ConnectFour/Assets/_Project/Scripts/ConnectFour.Tests/GameWorldTests.cs
+++ ConnectFour/Assets/_Project/Scripts/ConnectFour.Tests/GameWorldTests.cs
@@ -14,8 +14,8 @@ namespace ConnectFour.Tests
         private DiscObject _discTemplate;
         private GameWorld _gameWorld;
 
-        [SetUp]
-        public void SetUp()
+        [UnitySetUp]
+        public IEnumerator SetUp()
         {
             GameObject gameWorldGameObject = new GameObject(nameof(GameWorld));
             gameWorldGameObject.SetActive(false);
@@ -25,21 +25,7 @@ namespace ConnectFour.Tests
             _discTemplate = new GameObject(nameof(DiscObject)).AddComponent<DiscObject>();
             _discTemplate.transform.parent = _columnTemplate.transform;
             _columnTemplate.DiscTemplate = _discTemplate;
-        }
 
-        [TearDown]
-        public void TearDown()
-        {
-            Object.Destroy(_gameWorld);
-
-            _gameWorld = null;
-            _columnTemplate = null;
-            _discTemplate = null;
-        }
-
-        [UnityTest]
-        public IEnumerator SampleTest()
-        {
             _gameWorld.BoardSystem = new BoardSystem
             {
                 ColumnCount = 7,
@@ -59,16 +45,66 @@ namespace ConnectFour.Tests
 
             _gameWorld.gameObject.SetActive(true);
 
-            // skip 2 turns to ensure that both Awake and Start gets called
+            // skip 2 frames to ensure that both Awake and Start gets called
             yield return null;
             yield return null;
+        }
 
-            Assert.Fail();
-
+        [TearDown]
+        public void TearDown()
+        {
             foreach (Controller controllerAsset in _gameWorld.TurnSystem.ControllersAssets)
             {
                 Object.Destroy(controllerAsset);
             }
+
+            Object.Destroy(_gameWorld);
+
+            _gameWorld = null;
+            _columnTemplate = null;
+            _discTemplate = null;
+        }
+
+        [Test]
+        public void DrawFlow()
+        {
+            Click(3);
+            Assert.AreEqual(BoardSystem.MoveState.Valid, _gameWorld.LastMoveState);
+
+            for (int i = 0; i < _gameWorld.BoardSystem.ColumnCount; i++)
+            {
+                do
+                {
+                    Click(i);
+                }
+                while (_gameWorld.LastMoveState == BoardSystem.MoveState.Valid);
+            }
+
+            Assert.AreEqual(BoardSystem.MoveState.Draw, _gameWorld.LastMoveState);
+        }
+
+        [Test]
+        public void WinFlow()
+        {
+            for (int i = 0; i < _gameWorld.BoardSystem.ColumnCapacity; i++)
+            {
+                Click(0);
+                Assert.AreEqual(BoardSystem.MoveState.Valid, _gameWorld.LastMoveState);
+            }
+
+            Click(0);
+            Assert.AreEqual(BoardSystem.MoveState.Invalid, _gameWorld.LastMoveState);
+
+            for (int i = 1; i <= 2; i++)
+            {
+                Click(i);
+                Assert.AreEqual(BoardSystem.MoveState.Valid, _gameWorld.LastMoveState);
+                Click(i);
+                Assert.AreEqual(BoardSystem.MoveState.Valid, _gameWorld.LastMoveState);
+            }
+
+            Click(3);
+            Assert.AreEqual(BoardSystem.MoveState.Win, _gameWorld.LastMoveState);
         }
 
         private void Click(int columnIndex)
--- ConnectFour/Assets/_Project/Scripts/ConnectFour/GameWorld.cs
+++ ConnectFour/Assets/_Project/Scripts/ConnectFour/GameWorld.cs
@@ -66,7 +66,7 @@ namespace ConnectFour
 
                 case BoardSystem.MoveState.Invalid:
                 {
-                    Debug.Log("Invalid");
+                    _turnSystem.BeginTurn(controllerIndex);
 
                     break;
                 }
